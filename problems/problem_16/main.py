fa0580d2e74d258dce85e4e4b43e0c6b6b2a2156
Luan Oliveira <olivrluan@gmail.com>
2024-06-09 13:58:50 -0300

    feat: Append problem 16 (solved) to the project

---
+++ problems/problem_16/main.py
@@ -0,0 +1,4 @@
+# Shine On  You Crazy Diamond
+
+def sum_of_digits(power):
+    return sum([int(digit) for digit in str(2**power)])
---
+++ problems/problem_16/main_test.py
@@ -0,0 +1,6 @@
+# Test Here
+from main import sum_of_digits
+
+def test_sum_of_digits():
+    assert sum_of_digits(15) == 26
+    assert sum_of_digits(1000) == 1366
